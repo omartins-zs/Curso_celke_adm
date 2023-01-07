913702a593c63ca906263dc768bc683c58e5609a
omartins-zs <gabrielmatheus209@gmail.com>
2023-01-07 14:53:56 -0300

    Adicionando Controller de Nivel de Acesso

---
+++ app/adms/Controllers/NivelAcesso.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\adms\Controllers;
+
+/**
+ * Description of NivelAcesso
+ *
+ * @author Gabriel Matheus
+ */
+class NivelAcesso
+{
+    public function listAtivo() {
+        echo "Página listar Nivel de Acesso<br>";
+    }
+}
