8db306a4468f3782aea678f3f56d1905af903c09
omartins-zs <gabrielmatheus209@gmail.com>
2023-03-16 11:14:30 -0300

    Add Link CDN do jQuery

--- app/adms/Views/include/footer.php
+++ app/adms/Views/include/footer.php
@@ -1,3 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="<?= URLADM ?>app/adms/assets/js/personalizado_adms.js"></script>
 </body>
 
 </html>
