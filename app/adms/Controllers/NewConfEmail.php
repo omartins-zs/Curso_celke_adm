ead9ae07191d690d01e7c2c834107c4a6ae4887c
omartins-zs <gabrielmatheus209@gmail.com>
2024-01-09 09:36:43 -0300

    Criando Controller New Conf Email

---
+++ app/adms/Controllers/NewConfEmail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\adms\Controllers;
+
+/**
+ * Description of NewConfEmail
+ *
+ * @author Gabriel Matheus
+ */
+class NewConfEmail
+{
+
+    private $dados;
+    private $dadosForm;
+
+    public function index()
+    {
+    }
+}
