1fcc1a7657c79c1b98ff7f8790e71a5d3e69624f
omartins-zs <gabrielmatheus209@gmail.com>
2024-01-09 08:02:34 -0300

    Add Validação de Email e Permissão

--- app/adms/Models/AdmsLogin.php
+++ app/adms/Models/AdmsLogin.php
@@ -9,10 +9,9 @@ use PDO;
  *
  * @author Gabriel Matheus
  */
-class AdmsLogin extends helper\AdmsConn
+class AdmsLogin
 {
     private array $dados;
-    private object $conn;
     private $resultadoBd;
     private bool $resultado;
 
@@ -28,7 +27,7 @@ class AdmsLogin extends helper\AdmsConn
         $viewUser = new \App\adms\Models\helper\AdmsRead();
         //$viewUser->exeRead("adms_users", "WHERE user =:user LIMIT :limit", "user={$this->dados['user']}&limit=1");
         $viewUser->fullRead(
-            "SELECT id, name, nickname, email, password, image
+            "SELECT id, name, nickname, email, password, adms_sits_user_id, image
                 FROM adms_users
                 WHERE username =:username OR
                 email =:email
@@ -38,10 +37,26 @@ class AdmsLogin extends helper\AdmsConn
 
         $this->resultadoBd = $viewUser->getResult();
         if ($this->resultadoBd) {
-            $this->validarSenha();
+            $this->valEmailPerm();
         } else {
-            $_SESSION['msg'] = "Erro: Usuário não encontrado!<br><br>";
+            $_SESSION['msg'] = "Erro: Usuário não encontrado!<br>";
             $this->resultado = false;
+        }
+    }
+
+    private function valEmailPerm()
+    {
+        if ($this->resultadoBd[0]['adms_sits_user_id'] == 3) {
+            $_SESSION['msg'] = "Erro: Necessário confirmar o e-mail!<br>";
+            $this->resultado = false;
+        } elseif ($this->resultadoBd[0]['adms_sits_user_id'] == 5) {
+            $_SESSION['msg'] = "Erro: E-mail descadastrado, entre em contato com a empresa!<br>";
+            $this->resultado = false;
+        } elseif ($this->resultadoBd[0]['adms_sits_user_id'] == 2) {
+            $_SESSION['msg'] = "Erro: E-mail inativo, entre em contato com a empresa!<br>";
+            $this->resultado = false;
+        } else {
+            $this->validarSenha();
         }
     }
 
@@ -51,11 +66,11 @@ class AdmsLogin extends helper\AdmsConn
             $_SESSION['user_id'] = $this->resultadoBd[0]['id'];
             $_SESSION['user_name'] = $this->resultadoBd[0]['name'];
             $_SESSION['user_nickname'] = $this->resultadoBd[0]['nickname'];
-            $_SESSION['user_image'] = $this->resultadoBd[0]['image'];
             $_SESSION['user_email'] = $this->resultadoBd[0]['email'];
+            $_SESSION['user_image'] = $this->resultadoBd[0]['image'];
             $this->resultado = true;
         } else {
-            $_SESSION['msg'] = "Erro: Usuário ou senha incorreta! <br><br>";
+            $_SESSION['msg'] = "Erro: Usuário ou senha incorreta!<br>";
             $this->resultado = false;
         }
     }
